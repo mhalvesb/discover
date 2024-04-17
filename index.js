b5d30d85f3068e38a408e0c2a91b7deea3dab300
Matheus Rodrigues <118693886+mhalvesb@users.noreply.github.com>
2024-04-16 23:57:06 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+const button = document.querySelector(".click")
+const lightmode = document.querySelector("html");
+
+
+
+
+
+button.addEventListener("click", () =>{
+    lightmode.classList.toggle("light");
+})
+
+
